a56ee32237d770d3dba1fa11f181f9d7a13b6db0
aselbie <aselbie@gmail.com>
2014-10-01 17:33:12 -0700

    Completed routing and serving

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var _ = require('underscore');
+
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -10,9 +12,48 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+exports.send = send = function(res, body, code){
+  code = code || 200;
+  res.writeHead(code, headers);
+  res.end(body);
+}
+
+exports.serveLoading = serveLoading = function(res){
+  console.log('Trying to serve: ', path.join(__dirname, 'public', '/loading.html'));
+  fs.readFile(path.join(__dirname, 'public', '/loading.html'), function(err, data){
+    if (err){
+      send(res, null, 404);
+    } else {
+      headers.location = '/loading.html';
+      send(res, data, 302);
+    }
+  })
+}
+
+exports.serveSite = serveSite = function(res, asset, callback){
+  fs.readFile(path.join(__dirname, '../archives/sites', asset), function(err, data){
+    if (err){
+      callback(res);
+    } else {
+      send(res, data);
+    }
+  })
+}
+
+exports.servePublic = servePublic = function(res, asset, callback){
+  console.log('Trying to serve: ', path.join(__dirname, 'public', asset));
+  fs.readFile(path.join(__dirname, 'public', asset), function(err, data){
+    if (err){
+      serveSite(res, asset, callback);
+    } else {
+      send(res, data);
+    }
+  })
+}
+
+
 exports.serveAssets = function(res, asset, callback) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  servePublic(res, asset, callback);
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,40 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var parse = require('url').parse;
+var httpHelpers = require('./http-helpers.js');
+var qs = require('querystring');
 // require more modules/folders here!
+var actions = {
+  'GET': function(req, res){
+    var pathName = parse(req.url).pathname;
+    if (pathName === '/'){
+      pathName = '/index.html';
+    }
+    httpHelpers.serveAssets(res, pathName, function(res){
+      httpHelpers.send(res);
+    });
+  },
+  'POST': function(req, res){
+    var pathName = parse(req.url, true);
+    var body = '';
+    req.on('data', function(chunk){
+      body += chunk;
+    })
+    req.on('end', function(){
+      var postData = qs.parse(body);
+      // AsyncWorkerTask(postData);
+      httpHelpers.serveAssets(res, postData.url, function(res){
+        httpHelpers.serveLoading(res);
+      });
+    })
+    // httpHelpers.serveAssets(res, pathName, function(res){
+    //   ;
+    // });
+  }
+}
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (actions[req.method]){
+    actions[req.method](req, res);
+  }
 };
